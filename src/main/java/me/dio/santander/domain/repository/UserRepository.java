dd7871743b8cca8b851924080506e92c4fcf2240
Sergio M B <110953280+Mefest0@users.noreply.github.com>
2024-07-26 00:17:47 -0300

    Implementação de Tratamento de exceções em APIs Rest

--- src/main/java/me/dio/santander/domain/repository/UserRepository.java
+++ src/main/java/me/dio/santander/domain/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package me.dio.santander.domain.model.repository;
+package me.dio.santander.domain.repository;
 
 import me.dio.santander.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    boolean existsByAccountNumber(String accountNumber);
 }
